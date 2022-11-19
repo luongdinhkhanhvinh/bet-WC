f82149a041565b6d02c11b8593e576db8b6f6217
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2022-11-19 13:07:37 +0700

    finish code

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let router = express.Router();
-
-
-router.get("/admin/health", (request, response) => {
-  return response.json({ version: "ADMIN TBD" });
-});
-
-module.exports = router;
