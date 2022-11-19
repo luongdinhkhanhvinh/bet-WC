1d74b8c48d1831b5dcedee4ecedfa9186d13d848
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2022-11-19 18:39:06 +0700

    update

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// // var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// // app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-
 const express = require("express");
 const hbs = require("hbs");
 const session = require("express-session");
@@ -68,7 +37,6 @@ passport.deserializeUser(function (id, done) {
 
 hbs.registerPartials(__dirname + "/views/partials");
 
-
 const app = express();
 
 app.set("view engine", "hbs");
@@ -86,7 +54,7 @@ require("./hbs_helpers.js")();
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  // res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
